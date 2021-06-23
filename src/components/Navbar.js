266a4041b135f78f8edd82cfd99a6765bf1301ef
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2021-06-23 13:48:05 +0530

    Navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,23 @@
 import { PersonIcon, RepoIcon } from "@primer/octicons-react";
 
-function Navbar({ setTab }) {
+function Navbar({ tab, setTab }) {
   return (
-    <nav>
-      <button onClick={() => setTab("user")}>
-        <PersonIcon size={24} /> User
+    <nav className="flex justify-evenly bg-gray-800 h-16 p-3">
+      <button
+        className={`flex items-center text-white focus:outline-none py-2 px-5 rounded-md ${
+          tab === "user" ? "bg-gray-900" : null
+        }`}
+        onClick={() => setTab("user")}
+      >
+        <PersonIcon size={24} /> <span className="text-xl">User</span>
       </button>
-      <button onClick={() => setTab("repo")}>
-        <RepoIcon size={24} />
-        Repositories
+      <button
+        className={`flex items-center text-white focus:outline-none py-2 px-5 rounded-md ${
+          tab === "repo" ? "bg-gray-900" : null
+        }`}
+        onClick={() => setTab("repo")}
+      >
+        <RepoIcon size={24} /> <span className="text-xl">Repositories</span>
       </button>
     </nav>
   );
